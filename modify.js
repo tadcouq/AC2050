e93fce1b7f09fe8534a6bc37e443fa33c92c19c4
Nguyen Dat <88364090+tadcouq@users.noreply.github.com>
2025-01-03 21:29:25 +0700

    030124

--- modify.js
+++ modify.js
@@ -36,3 +36,13 @@ export function modify(cmd, mssv, cpa){
     let command = prompt("Nhấn phím Enter để tiếp tục");
     return;
 };
+
+/* Độ phức tạp của thuật toán
+- Độ phức tạp của thuật toán là O(n) với n là số lượng sinh viên trong file dataStudent.json
+
+- Vòng lặp for chạy qua từng phần tử trong mảng arrSV có độ dài arrlength (o(n))
+- Trong trường hợp tìm thấy sinh viên có mã số sinh viên mssv thì cập nhật điểm CPA và cảnh cáo của sinh viên đó (O(1))
+- Nếu không tìm thấy sinh viên có mã số sinh viên mssv thì thông báo không tìm thấy (O(1))
+- Ghi lại dữ liệu mới vào file dataStudent.json (O(1))
+- Tổng cộng độ phức tạp của thuật toán là O(n)
+*/
